e2cb09f01a56ce6cdf3ad24ff6b9386a3fbd8824
shakil <mdshakilhossain720@gmail.com>
2025-02-11 21:43:50 +0600

    hive curedu

--- lib/view/homepage/home_page.dart
+++ lib/view/homepage/home_page.dart
@@ -8,6 +8,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController nametextEditingController =
+      TextEditingController();
+  final TextEditingController quantitytextEditingController =
+      TextEditingController();
+
+  // create new item
+  Future<void> createItem(Map<String, dynamic> item) async {
+    final item = Item(
+        name: nametextEditingController.text,
+        quantity: quantitytextEditingController.text);
+    await Hive.box<Item>('items').add(item);
+  }
+
+  void showmethod(BuildContext context, int? itemkey) {
+    showModalBottomSheet(
+        elevation: 0,
+        isScrollControlled: true,
+        context: context,
+        builder: (_) {
+          return Container(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+              left: 15,
+              right: 15,
+              top: 15,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                TextField(
+                  controller: nametextEditingController,
+                  decoration: const InputDecoration(hintText: "Name"),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                TextField(
+                  controller: quantitytextEditingController,
+                  decoration: const InputDecoration(hintText: "Quantity"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      nametextEditingController.text = "";
+                      quantitytextEditingController.text = "";
+                      Navigator.pop(context);
+                    },
+                    child: const Text("create New")),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +72,7 @@ class _HomePageState extends State<HomePage> {
         title: const Text("Hive Page"),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => showmethod(context, null),
         child: const Icon(Icons.add),
       ),
     );
